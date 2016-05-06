912712f985e03914ebe2e66e6c478c3e3db5bd2a
Eyeye <christianfox890@icloud.com>
2016-05-06 09:25:29 +0100

    Installs cocoapod into Example project.

---
+++ Example/Pods/Headers/Private/KFXLocalSystemReporter/KFXHardwareReporter.h
@@ -0,0 +1,1 @@
+../../../../../KFXLocalSystemReporter/Classes/KFXHardwareReporter.h
--- Example/Pods/Target Support Files/KFXLocalSystemReporter/KFXLocalSystemReporter-umbrella.h
+++ Example/Pods/Target Support Files/KFXLocalSystemReporter/KFXLocalSystemReporter-umbrella.h
@@ -1,5 +1,8 @@
 #import <UIKit/UIKit.h>
 
+#import "KFXDeviceActivityReporter.h"
+#import "KFXHardwareReporter.h"
+#import "KFXSoftwareReporter.h"
 
 FOUNDATION_EXPORT double KFXLocalSystemReporterVersionNumber;
 FOUNDATION_EXPORT const unsigned char KFXLocalSystemReporterVersionString[];
